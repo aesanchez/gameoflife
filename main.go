da3f7254c9eba6c31c80a56ba0eed4a98a9db0c3
Agustin Sanchez <agustinesanchez23@gmail.com>
2020-03-22 20:27:11 -0300

    refactor

--- main.go
+++ main.go
@@ -1,15 +1,8 @@
 package main
 
-// The following are the rules for Conway's game of life
-// * Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-// * Any live cell with two or three live neighbours lives on to the next generation.
-// * Any live cell with more than three live neighbours dies, as if by overpopulation.
-// * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
 import (
 	"fmt"
 	"image/color"
-	"math/rand"
 	"time"
 
 	"github.com/faiface/pixel"
@@ -26,32 +19,6 @@ var (
 )
 
 type Matrix [][]int
-
-type LifeInput struct {
-	Cells        Matrix
-	RowOffset    int
-	ColumnOffset int
-}
-
-var lifeInput1 = LifeInput{
-	Cells: Matrix{
-		[]int{1, 0, 0},
-		[]int{0, 1, 1},
-		[]int{1, 1, 0},
-	},
-	RowOffset:    5,
-	ColumnOffset: 5,
-}
-
-var lifeInput2 = LifeInput{
-	Cells: Matrix{
-		[]int{1, 0, 0},
-		[]int{0, 1, 1},
-		[]int{1, 1, 0},
-	},
-	RowOffset:    15,
-	ColumnOffset: 5,
-}
 
 type Game struct {
 	Input  Matrix
@@ -90,7 +57,8 @@ func run(g *Game) {
 				if cell == 0 {
 					continue
 				}
-				imd.Color = colornames.Map[colornames.Names[rand.Intn(len(colornames.Names))]]
+				imd.Color = colornames.Black
+				// imd.Color = colornames.Map[colornames.Names[rand.Intn(len(colornames.Names))]]
 				start := pixel.V(float64(c)*cellWidth, windowSize-float64(r)*cellWidth)
 				imd.Push(start, pixel.V(start.X+cellWidth, start.Y+cellWidth))
 				imd.Rectangle(0)
@@ -128,26 +96,30 @@ func run(g *Game) {
 	}
 }
 
-func main() {
-	game := &Game{}
+func NewGame() *Game {
+	game := Game{}
 	game.Input = make(Matrix, N)
 	game.Output = make(Matrix, N)
 	for i := 0; i < N; i++ {
 		game.Output[i] = make([]int, N)
 		game.Input[i] = make([]int, N)
 	}
+	return &game
+}
 
-	for r, cells := range lifeInput1.Cells {
+func (g *Game) LoadLifeInput(i LifeInput) {
+	for r, cells := range i.Cells {
 		for c, cell := range cells {
-			game.Input[lifeInput1.RowOffset+r][lifeInput1.ColumnOffset+c] = cell
+			g.Input[i.RowOffset+r][i.ColumnOffset+c] = cell
 		}
 	}
+}
 
-	for r, cells := range lifeInput2.Cells {
-		for c, cell := range cells {
-			game.Input[lifeInput2.RowOffset+r][lifeInput2.ColumnOffset+c] = cell
-		}
-	}
+func main() {
+	game := NewGame()
+	game.LoadLifeInput(glider)
+	glider.RowOffset += 10
+	game.LoadLifeInput(glider)
 
 	pixelgl.Run(func() { run(game) })
 }
