7a4cd37c7db4dc8423df09e55cf6f32c2e41aa21
Agustin Sanchez <agustinesanchez23@gmail.com>
2020-03-22 15:08:53 -0300

    canvas testing

--- canvas.go
+++ canvas.go
@@ -1,11 +1,5 @@
 package main
 
-// The following are the rules for Conway's game of life
-// * Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-// * Any live cell with two or three live neighbours lives on to the next generation.
-// * Any live cell with more than three live neighbours dies, as if by overpopulation.
-// * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
 import (
 	"time"
 
